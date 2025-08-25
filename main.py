a2a3d0b058cc6750f2f85b668182a3f7a0fd990f
Debkumar_Baksi <sg383332@gmail.com>
2025-08-25 11:44:58 +0530

    third commit

--- main.py
+++ main.py
@@ -102,4 +102,6 @@ iface = gr.Interface(
 )
 
 if __name__ == "__main__":
-    iface.launch()
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    iface.launch(server_name="0.0.0.0", server_port=port)
